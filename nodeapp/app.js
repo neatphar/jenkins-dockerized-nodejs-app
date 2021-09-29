885128b5cab12f56a55e5374eb69a087d8ef28bb
Omar Aboelmagd <62209107+neatphar@users.noreply.github.com>
2021-09-29 18:10:42 +0200

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('<h1>Hello World!</h1> <span>Github!!!</span>')
 })
   
 app.listen(port, () => {
